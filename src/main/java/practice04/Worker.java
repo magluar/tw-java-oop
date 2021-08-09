b20d8b27727a55245d1c85b87f33b1c1b986bf4e
magluar <aryann.magluyan@oocl.com>
2021-08-09 22:43:57 +0800

    [PRACTICE04][MAGLUAR] Feat: should_worker_introduce_itself_with_no_field

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -6,6 +6,6 @@ public class Worker extends Person {
     }
 
     public String introduce() {
-        return "I am a Worker. I have a job.";
+        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Worker. I have a job.";
     }
 }
