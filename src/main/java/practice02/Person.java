0c363af9f8878efca23390a07f9cd5c7ad50e35a
magluar <aryann.magluyan@oocl.com>
2021-08-09 21:45:36 +0800

    [PRACTICE02][MAGLUAR] Feat: should_person_have_an_introduce_method_which_introduce_person_with_name_and_age

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -16,4 +16,8 @@ public class Person {
     public int getAge() {
         return age;
     }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
 }
