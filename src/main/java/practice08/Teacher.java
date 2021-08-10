3f566d0bb5ff4982e2af742a95f7310bff04d98d
magluar <aryann.magluyan@oocl.com>
2021-08-10 23:13:56 +0800

    [PRACTICE08][MAGLUAR] Feat: should_teacher_introduce_itself_with_no_class_teaching

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -9,12 +9,20 @@ public class Teacher extends Person{
         this.klass = klass;
     }
 
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        this.klass = null;
+    }
+
     public Klass getKlass() {
         return this.klass;
     }
 
     public String introduce(){
-        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach Class " +
-                getKlass().getNumber() + ".";
+        if (this.klass != null){
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach Class " +
+                    getKlass().getNumber() + ".";
+        }
+        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach No Class.";
     }
 }
