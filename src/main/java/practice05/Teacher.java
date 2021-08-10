4c79921e0f248dd1aa72732203a141501f2fe4c2
magluar <aryann.magluyan@oocl.com>
2021-08-10 14:57:35 +0800

    [PRACTICE05][MAGLUAR] Feat: should_teacher_introduce_itself_with_which_class_it_teaches

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,18 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+    private final int klass;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach Class " + klass + ".";
+    }
 }
