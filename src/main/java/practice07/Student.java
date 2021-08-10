94bd693114d56d1baffb7f4e8e3ea7190011f8a6
magluar <aryann.magluyan@oocl.com>
2021-08-10 22:04:30 +0800

    [PRACTICE07][MAGLUAR] Feat: should_student_introduce_itself_as_class_leader

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -26,6 +26,11 @@ public class Student {
     }
 
     public String introduce() {
+        if (getKlass().getLeader() != null) {
+//            "My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.";
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Student. I am Leader of " +
+                    "Class " + klass.getNumber() + ".";
+        }
         return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Student. I am at Class "
                 + klass.getNumber() + ".";
     }
