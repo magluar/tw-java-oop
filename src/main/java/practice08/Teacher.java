0d8342091728bc0f7cdd483331129d092a27c72f
magluar <aryann.magluyan@oocl.com>
2021-08-10 23:10:27 +0800

    [PRACTICE08][MAGLUAR] Feat: should_teacher_have_name_and_age_and_class

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,15 @@
 package practice08;
 
-public class Teacher {
+
+public class Teacher extends Person{
+    private final Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
 }
