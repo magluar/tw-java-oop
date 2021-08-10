545582bfde845744b24cfc15dcc74d3fe19a81c4
magluar <aryann.magluyan@oocl.com>
2021-08-10 23:58:16 +0800

    [PRACTICE09][MAGLUAR] Feat: should_class_get_display_name

--- src/test/java/practice09/Practice09Test.java
+++ src/test/java/practice09/Practice09Test.java
@@ -44,11 +44,11 @@ public class Practice09Test {
     public void should_class_have_a_number() throws Exception {
         assertThat(klass.getNumber()).isEqualTo(2);
     }
-//
-//    @Test
-//    public void should_class_get_display_name() throws Exception {
-//        assertThat(klass.getDisplayName()).isEqualTo("Class 2");
-//    }
+
+    @Test
+    public void should_class_get_display_name() throws Exception {
+        assertThat(klass.getDisplayName()).isEqualTo("Class 2");
+    }
 //
 //    @Test
 //    public void should_class_not_assign_a_student_as_leader_when_student_is_not_a_member() throws Exception {
