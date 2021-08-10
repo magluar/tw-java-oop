eea28f90eb7a5a0635fc00686ab62b03ae099705
magluar <aryann.magluyan@oocl.com>
2021-08-10 15:21:15 +0800

    [PRACTICE06][MAGLUAR] Feat: should_student_introduce_with_class

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -15,4 +15,9 @@ public class Student extends Person{
     public Klass getKlass(){
         return this.klass;
     }
+
+    public String introduce() {
+        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Student. I am at Class "
+                + klass.getNumber() + ".";
+    }
 }
