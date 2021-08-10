b82a75c2120be91cb8499149990eae6308f5f0e7
magluar <aryann.magluyan@oocl.com>
2021-08-10 15:18:28 +0800

    [PRACTICE06][MAGLUAR] Feat: should_student_have_name_age_and_class_number

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,18 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass(){
+        return this.klass;
+    }
 }
