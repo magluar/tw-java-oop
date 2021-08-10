88dee3a0f21f1728b03615b5f7d905c1604569cd
magluar <aryann.magluyan@oocl.com>
2021-08-10 22:24:33 +0800

    [PRACTICE08][MAGLUAR] Feat: should_person_have_id_name_and_age

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,9 @@
 package practice08;
 
 public class Klass {
+    private final int klass;
+
+    public Klass(int klass) {
+        this.klass = klass;
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,21 @@
 package practice08;
 
 public class Person {
+    private final String name;
+    private final int age;
+    private final int id;
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
