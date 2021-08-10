b9a6ac61c8fd5b82a225a28bef364394d0f326de
magluar <aryann.magluyan@oocl.com>
2021-08-11 00:11:19 +0800

    [PRACTICE09][MAGLUAR] Feat: should_teacher_have_name_and_age_and_classes

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,17 @@
 package practice09;
 
-public class Teacher {
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private final LinkedList<Klass> classList;
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> linkedList) {
+        super(id, name, age);
+        this.classList = linkedList;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return this.classList;
+    }
 }
