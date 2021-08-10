a0a9fce5338fe11ddeef7c54d20d26da3ce9f042
magluar <aryann.magluyan@oocl.com>
2021-08-10 22:10:07 +0800

    [PRACTICE07][MAGLUAR] Feat: should_teacher_have_name_and_age_and_class

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,24 +1,11 @@
 package practice07;
 
-public class Student {
-    private final int id;
-    private final String name;
-    private final int age;
+public class Student extends Person{
     private final Klass klass;
 
     public Student(int id, String name, int age, Klass klass) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+        super(id, name, age);
         this.klass = klass;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAge() {
-        return this.age;
     }
 
     public Klass getKlass() {
