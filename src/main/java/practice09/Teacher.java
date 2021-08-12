20eaec1a1a2878ebbef43b40abb041ea4944853b
magluar <aryann.magluyan@oocl.com>
2021-08-12 23:31:10 +0800

    [PRACTICE09][MAGLUAR] test: should_teacher_introduce_itself_with_which_classes_it_teaches, should_teacher_introduce_itself_with_no_class_teaching

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -2,6 +2,7 @@ package practice09;
 
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 public class Teacher extends Person{
     private final LinkedList<Klass> classList;
@@ -11,7 +12,23 @@ public class Teacher extends Person{
         this.classList = linkedList;
     }
 
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        this.classList = null;
+    }
+
     public LinkedList<Klass> getClasses() {
         return this.classList;
+    }
+//My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2, 3.
+    public String introduce() {
+        if (classList != null) {
+            String numOfClasses = classList.stream()
+                    .map(klass -> String.valueOf(klass.getNumber()))
+                    .collect(Collectors.joining(", "));
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach Class " +
+                    numOfClasses + ".";
+        }
+        return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach No Class.";
     }
 }
